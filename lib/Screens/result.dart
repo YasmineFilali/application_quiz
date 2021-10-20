e99572eddc81e27d54c32d0d4fa1845ccefc7108
yasmine filali <55030252+YasmineFilali@users.noreply.github.com>
2021-10-20 18:35:57 +0200

    Update result.dart

--- lib/Screens/result.dart
+++ lib/Screens/result.dart
@@ -21,18 +21,18 @@ class _ResultsState extends State<Results> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Correct Answer / Total : ${widget.correct}/${widget.total}", style: TextStyle(fontSize: 25),),
+              Text("Réponses correctes / Total : ${widget.correct}/${widget.total}", style: TextStyle(fontSize: 25),),
               SizedBox(height: 8,),
-              Text("You answered correctly : ${widget.correct}", style: TextStyle(fontSize: 16, color: Colors.green), textAlign: TextAlign.center,),
+              Text("Bonne réponse : ${widget.correct}", style: TextStyle(fontSize: 16, color: Colors.green), textAlign: TextAlign.center,),
               SizedBox(height: 8,),
-              Text("Your incorrect answer : ${widget.incorrect}", style: TextStyle(fontSize: 16, color: Colors.red), textAlign: TextAlign.center,),
+              Text("Mauvaise réponse : ${widget.incorrect}", style: TextStyle(fontSize: 16, color: Colors.red), textAlign: TextAlign.center,),
 
               SizedBox(height: 20,),
               GestureDetector(
                   onTap: (){
                     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Home()));
                   },
-                  child: orangeButton(context, "Go To Home", MediaQuery.of(context).size.width/2, Colors.blueAccent)
+                  child: orangeButton(context, "Retour a l’accueil ", MediaQuery.of(context).size.width/2, Colors.blueAccent)
               ),
             ],
           ),
